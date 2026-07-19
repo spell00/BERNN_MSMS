8326ccd9ad315b15f545e650cfc0449bbd293b4d
Simon Pelletier <simonjpelletier@gmail.com>
2026-07-19 01:14:35 +0000

    bump version to 0.6.1 (head_classifier, ae_head_predictor, train_ae_head_sweep, full sklearn+XGBoost sweep)

--- setup.py
+++ setup.py
@@ -313,7 +313,7 @@ special_requirements = [
 
 setup(
     name='bernn',
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     url='https://github.com/spell00/BERNN_MSMS',  # Replace with actual repo URL
     license='MIT',  # Choose appropriate license
