ebb96d5ce3827123dbaab629749107767aa46b74
Simon Pelletier <simonjpelletier@gmail.com>
2026-07-29 17:41:02 +0000

    Release bernn 0.6.22

--- setup.py
+++ setup.py
@@ -313,7 +313,7 @@ special_requirements = [
 
 setup(
     name='bernn',
-    version='0.6.21',
+    version='0.6.22',
     packages=find_packages(),
     url='https://github.com/spell00/BERNN_MSMS',  # Replace with actual repo URL
     license='MIT',  # Choose appropriate license
@@ -437,5 +437,4 @@ setup(
         ],
     }
 )
-
 
